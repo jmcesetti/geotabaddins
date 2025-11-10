980409ce93e59668b2347dff9305a4703b59f8b3
jmcesetti-source <jmcesetti@gmail.com>
2025-11-10 11:02:28 -0300

    Update tripExporter.js

--- tripExporter.js
+++ tripExporter.js
@@ -1,16 +1,4 @@
-// tripExporter.js
-
 geotab.customButtons.tripExporter = async (event, api, state) => {
-  'use strict';
-
-  event.preventDefault();
-
-  try {
-    console.log("✅ Botón Exportar Viaje presionado!");
-    // Abre una nueva pestaña en Chrome
-    window.open("https://www.google.com", "_blank");
-  } catch (error) {
-    console.error("❌ Error ejecutando tripExporter:", error);
-    alert("Ocurrió un error al ejecutar el botón.");
-  }
+  console.log("✅ Botón Exportar Viaje presionado!");
+  window.open("https://www.google.com", "_blank");
 };
