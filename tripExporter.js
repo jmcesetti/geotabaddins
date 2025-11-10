eaa1b894caaaf09e560a2ee532773902d4b7578a
jmcesetti <jmcesetti@gmail.com>
2025-11-10 12:04:12 -0300

    Update tripExporter.js

--- tripExporter.js
+++ tripExporter.js
@@ -1,14 +1,1 @@
-geotab.addin.tripExporter = function(api, state) {
-  return {
-    initialize: function(api, state, callback) {
-      callback(); // Obligatorio: le dice a Geotab que el add-in está listo
-    },
-    focus: function(api, state) {
-      alert("✅ Add-In cargado correctamente");
-      window.open("https://www.google.com", "_blank");
-    },
-    blur: function() {
-      // opcional: cuando el usuario sale de la página
-    }
-  };
-};
+
