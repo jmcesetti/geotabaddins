b79b3c15ce0929f3895677cb482bb3fcfadb4b71
jmcesetti <jmcesetti@gmail.com>
2026-07-23 13:19:16 -0300

    done

--- SRF Positions/addin.js
+++ SRF Positions/addin.js
@@ -1,26 +1,32 @@
-geotab.addin.redCircle = (elt, service) => {
-    elt.innerHTML = '<div class="addin"><h3>Red Circle Map</h3><p class="info">Círculo rojo en coordenadas -38.2112142, -69.2293903. Muévete en el mapa y haz zoom para verlo interactuar.</p></div>';
-
-    // Draw red circle at specific coordinates
-    service.canvas.circle(
-        { lat: -38.2112142, lng: -69.2293903 },
-        50,     // z-index
-        40      // size (scales with zoom)
-    ).change({
-        "fill": "#FF0000",
-        "fill-opacity": 0.7,
-        "r": 10
-    }).attach("click", () => {
-        console.log("Circle clicked at -38.2112142, -69.2293903");
-    }).attach("over", () => {
-        console.log("Mouse over circle");
-    }).attach("out", () => {
-        console.log("Mouse left circle");
-    });
-
+geotab.addin.srfPositions = function(api, state) {
     return {
-        initialize: () => {},
-        focus: () => {},
-        blur: () => {}
+        initialize: function(api, state, callback) {
+            callback();
+        },
+        focus: function(api, state, callback) {
+            const mapElement = document.getElementById('mapDiv');
+            if (!mapElement) return;
+
+            mapElement.innerHTML = '<div class="addin"><h3>SRF Positions</h3><p class="info">Posiciones en coordenadas -38.2112142, -69.2293903. Muévete en el mapa y haz zoom para verlas interactuar.</p></div>';
+
+            api.canvas.circle(
+                { lat: -38.2112142, lng: -69.2293903 },
+                50,
+                40
+            ).change({
+                "fill": "#FF0000",
+                "fill-opacity": 0.7,
+                "r": 10
+            }).attach("click", () => {
+                console.log("Circle clicked at -38.2112142, -69.2293903");
+            }).attach("over", () => {
+                console.log("Mouse over circle");
+            }).attach("out", () => {
+                console.log("Mouse left circle");
+            });
+
+            if (callback) callback();
+        },
+        blur: function() {}
     };
 };
